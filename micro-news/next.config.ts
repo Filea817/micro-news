599737898ddb8eb64fca0c0d9bac00747e2d0233
Filea817 <filea817@gmail.com>
2025-04-05 16:05:37 +0900

    news1

--- micro-news/next.config.ts
+++ micro-news/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
 
 export default nextConfig;
